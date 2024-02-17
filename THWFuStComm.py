5cc8489c71cc6c625b8c4cc6039700aa9e6846ee
Dimitrios Skodras <dimitrios.skodras@tu-dortmund.de>
2024-02-18 00:13:24 +0100

    real-time chat between Funker clients with local socket.io.js script (not needing internet connection)

--- THWFuStComm.py
+++ THWFuStComm.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_socketio import SocketIO
 
 
 from src.funker import Funker
@@ -13,7 +14,7 @@ class THWFuStComm:
     def setup_routes(self):
         self.app.route("/")(self.home)
         self.app.route("/login", methods=['POST'])(self.login)
-        self.app.add_url_rule('/<role>', view_func=self.main)
+        self.app.add_url_rule('/<role>', view_func=self.select_role)
 
     def home(self):
         return render_template('index.html')
@@ -28,16 +29,28 @@ class THWFuStComm:
             self.agents["ldf"].append(LdF(user_sign))
 
         print(f"Received from client: {user_sign}, {user_pw}, {user_role}")
-        return redirect(url_for('main', role=user_role))
+        return redirect(url_for('select_role', role=user_role))
         #return render_template('index.html', message=f"Login: {user_sign} als {user_role}")
     
-    def main(self, role):
+    def select_role(self, role):
         print(self.agents)
         if role == "Funker":
             return render_template('funker.html')
+            
+    def handle_message(self,message):
+        print("Received message:",message)
+        self.socket_io.emit('message',message)
+
+        
+
     
     def run(self):
-        self.app.run(host='0.0.0.0', port=5000, debug=True)
+        self.socket_io = SocketIO(self.app, cors_allowed_origins="*")
+        @self.socket_io.on('message')
+        def handle_message(message):
+            self.handle_message(message)
+        #self.app.run(host='0.0.0.0', port=5000, debug=True)
+        self.socket_io.run(self.app,host='0.0.0.0', port=5000, debug=True)
 
 if __name__ == '__main__':
     THWFuStComm_app = THWFuStComm()
